205d7893808b9b16284798667c407a9ddeedd783
MarcosEsau <marcos.esau@outlook.com>
2023-05-20 19:59:29 -0300

    Add Edit Info feature

--- pages/characterPage.js
+++ pages/characterPage.js
@@ -4,17 +4,22 @@ import {app, db, firebaseConfig} from '../lib/firebase'
 import styles from "./characterPage.module.css";
 import { useSession, signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 const CreateCharacter = () => {
   const { data: session, status } = useSession();
-  const [character, setCharacter] = useState(null);
   const [autoLoginDone, setAutoLoginDone] = useState(false);
+  const [character, setCharacter] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+  const [name, setName] = useState("");
+  const [bloodType, setBloodType] = useState("");
+  const [system, setSystem] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
   useEffect(() => {
     if (status === "loading") {
       return;
     }
-
     if (!session && !autoLoginDone) {
       signIn('google');
       console.log('Not Logged In');
@@ -29,24 +34,37 @@ const CreateCharacter = () => {
   
   useEffect(() => {
     const fetchCharacter = async () => {
-      try {
-        const characterRef = doc(db, "users", session.user.email, "characters", characterCode);
-        const characterSnapshot = await getDoc(characterRef);
-
-        if (characterSnapshot.exists()) {
-          const characterData = characterSnapshot.data();
-          setCharacter(characterData);
-        } else {
-          console.log("Documento não encontrado");
-          return <div>Personagem não encontrado!</div>;
-        }
-      } catch (error) {
-        console.error("Erro ao buscar os dados do Firestore:", error);
+      const characterRef = doc(db, "users", session.user.email, "characters", characterCode);
+      const characterDoc = await getDoc(characterRef);
+      if (characterDoc.exists()) {
+        const characterData = characterDoc.data();
+        setCharacter(characterData);
+        setName(characterData.name);
+        setSystem(characterData.system);
+        setImageUrl(characterData.imageUrl);
       }
     };
 
     fetchCharacter();
-  }, [session, characterCode]);
+  }, [session, characterCode, editMode]);
+
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
+  const handleApplyChanges = async () => {
+    const characterRef = doc(db, "users", session.user.email, "characters", characterCode);
+    await setDoc(characterRef, {
+      name,
+      bloodType,
+      system,
+      imageUrl,
+    });
+    setEditMode(false);
+  };
+
+  
+
+  
 
   if (!character) {
     return <div>Carregando...</div>;
@@ -54,11 +72,49 @@ const CreateCharacter = () => {
 
   return (
     <div>
-      <h1>Personagem</h1>
-      <div className={styles.name}>
-        <h2>Nome:</h2>
-        <h1>{character.name}</h1>
-      </div>
+      {editMode ? (
+        <div className={styles.title}>
+          <h1>Alterar</h1>
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="text" value={bloodType} onChange={(e) => setBloodType(e.target.value)} />
+          <input type="text" value={system} onChange={(e) => setSystem(e.target.value)} />
+          <input type="text" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+          <button onClick={handleApplyChanges}>Aplicar Alterações</button>
+        </div>
+      ) : (
+        <div>
+          <h1 className={styles.title}>Personagem</h1>
+
+          <div className={styles.container}>
+
+            <img className={styles.img} src={character.imageUrl}></img>
+
+            <div className={styles.containerinfo}>
+
+              <div className={styles.name}>
+                <p>Nome:</p>
+                <h1>{character.name}</h1>
+              </div>
+
+              <div className={styles.name}>
+                <p>Sangue:</p>
+                <h1>{character.bloodType}</h1>
+              </div>
+
+              <div className={styles.name}>
+                <p>Sistema:</p>
+                <h1>{character.system}</h1>
+              </div>
+
+            </div>
+
+          </div>
+
+          <p>Sangue: {character.bloodType}</p>
+          <p>Sistema: {character.system}</p>
+          <button onClick={handleEditClick}>Editar</button>
+        </div>
+      )}
     </div>
   );
 };
