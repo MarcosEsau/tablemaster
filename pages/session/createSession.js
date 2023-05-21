1787e29ac726d5c172c1485c6e20644f89cf2f53
MarcosEsau <marcos.esau@outlook.com>
2023-05-21 00:33:56 -0300

    Feat: Session Creating to Real Time DB

---
+++ pages/session/createSession.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { collection, addDoc, setDoc, doc, getDoc, getCollection, query, where, onSnapshot, refEqual } from "firebase/firestore";
+import {app, db, firebaseConfig} from '../../lib/firebase'
+import styles from "../createCharacter.module.css";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+const CreateSession = () => {
+  const [id, setId] = useState('');
+  const [passkey, setPasskey] = useState('');
+  
+
+  const { data: session } = useSession();
+
+  const router = useRouter();
+
+  const handleCreateSession = async (user) => {
+    if (id == '' || passkey == '') {
+        alert('Preencha os campos corretamente')
+    } else {
+    try { 
+      const ref = doc(db, 'sessions', id);
+      const docSnap = await getDoc(ref); //Get Document
+      if (!docSnap.exists()) {
+        //If code doesn't exist, then create it
+        await setDoc(ref, {
+            name: id,
+            passkey: passkey,
+            owner: user.email,
+        });
+        router.push({
+          pathname: '/session/sessionPage',
+          query: {
+            id: id,
+          }
+        });
+      } else {
+        alert('Existe uma sala ativa com esse nome.')
+      }
+    } catch (error) {
+      console.error("Error creating session:", error);
+    }
+}
+  };
+  
+  return (
+    <>
+    <h1 className='big-title'>Criar Sessão</h1>
+    <div className={styles.creatorContainer}>
+      <form className={styles.creatorForm}>
+      <div className={styles.inputContainer}>
+        <label className={styles.label}>
+          Nome da Sessão:
+          <input
+            type="text"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            className={styles.input}
+          />
+        </label>
+      </div>
+      <div className={styles.inputContainer}>
+        <label className={styles.label}>
+          Senha:
+          <input
+            type="text"
+            value={passkey}
+            onChange={(e) => setPasskey(e.target.value)}
+            className={styles.input}
+          />
+        </label>
+      </div>
+        <button
+          type="button"
+          onClick={() => handleCreateSession(session.user)}
+          className={styles.button}
+        >
+          Criar Sessão
+        </button> 
+      </form>
+      
+    </div>
+    </>
+  );
+};
+
+export default CreateSession;
