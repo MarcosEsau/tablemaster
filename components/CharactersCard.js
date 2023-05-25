3265d6b1ee9e5002313c9fece9686314a700cf48
MarcosEsau <marcos.esau@outlook.com>
2023-05-24 22:38:13 -0300

    Style: Add Default image for img errors

--- components/CharactersCard.js
+++ components/CharactersCard.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import styles from './CharacterCard.module.css';
+import styles from './CharactersCard.module.css';
+import Button from '@mui/material/Button'
+import DeleteIcon from '@mui/icons-material/Delete';
 
-const CharacterCard = ({ name, bloodType, image, system = 'GLASS', characterCode }) => {
+const CharactersCard = ({ character }) => {
   const router = useRouter();
+  const [imageError, setImageError] = useState(false)
 
-  const handleClick = () => {
-    router.push(`/characters/${characterCode}`);
+  const handleImageError = () => {
+    setImageError(true)
   }
+
 
   return (
     <div className={styles.card}>
-      <img src={image} alt={`${name} character`} className={styles.image} />
+      {imageError ? (
+        <img src="/default.jpg" alt="Default" className={styles.image}/>
+      ) : (
+        <img src={character.imageUrl} alt={`${character.name} character`} className={styles.image} onError={handleImageError} />
+      )}
       <div className={styles.content}>
-        <h2 className={styles.name}>{name}</h2>
-        <p className={styles.bloodType}>Blood type: {bloodType}</p>
-        <p className={styles.system}>System: {system}</p>
-        <button className={styles.button} onClick={handleClick}>Access Character</button>
+        <h2 className={styles.name}>{character.name}</h2>
+        <p className={styles.bloodType}>Blood type: {character.bloodType}</p>
+        <p className={styles.system}>System: {character.system}</p>
+        <Button variant="contained">Entrar em Sessão</Button>
+          <Button variant="contained" color='success'>Acessar Personagem</Button>
+          <Button variant="outlined" color="secondary" startIcon={<DeleteIcon />}>
+            Deletar
+          </Button>
       </div>
     </div>
   );
 }
 
-export default CharacterCard;
+export default CharactersCard;
